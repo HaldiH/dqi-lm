16e7570877b49b5cafbda820f0c023f55c49c120
Hugo Haldi <20846785+HaldiH@users.noreply.github.com>
2026-01-05 13:43:09 +0100

    fix: update job array range from 1-6 to 1-8 in launch_eval_array.sh and launch_train_array.sh

--- slurm/launch_eval_array.sh
+++ slurm/launch_eval_array.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --constraint="COMPUTE_CAPABILITY_7_5|COMPUTE_CAPABILITY_8_0|COMPUTE_CAPABILITY_8_6|COMPUTE_CAPABILITY_8_9"
 #SBATCH --partition=shared-gpu
-#SBATCH --array=1-6
+#SBATCH --array=1-8
 #SBATCH --time=02:00:00
 #SBATCH --mem=16G
 
