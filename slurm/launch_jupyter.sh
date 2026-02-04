f502c3da5c7b2536699f053431bf2d5baa46374e
Hugo Haldi <20846785+HaldiH@users.noreply.github.com>
2026-02-04 12:47:19 +0100

    fix: Update job name and command for launching Jupyter Lab in SLURM script

--- slurm/launch_jupyter.sh
+++ slurm/launch_jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=jupyter-notebook
+#SBATCH --job-name=jupyter-lab
 #SBATCH --output=logs/jupyter-%j.out
 #SBATCH --error=logs/jupyter-%j.err
 #SBATCH --nodes=1
@@ -18,12 +18,14 @@ mkdir -p logs outputs
 APPTAINER_IMAGE="$HOME/scratch/containers/unsloth.sif"
 SCRATCH_DIR="$(realpath ~/scratch)"
 HF_HOME="$SCRATCH_DIR/huggingface"
-IP=$SLURMD_NODENAME
+IP=localhost
 PORT=8888
+
+echo "Starting Jupyter Lab on $IP:$PORT on node $SLURMD_NODENAME ..."
 
 srun apptainer exec \
   --nv \
   --env-file .env \
   --env HF_HOME=$HF_HOME \
   --mount type=bind,source=$SCRATCH_DIR,destination=$SCRATCH_DIR $APPTAINER_IMAGE \
-    uv run jupyter notebook --no-browser --ip=$IP --port=$PORT
+    uv run jupyter lab --no-browser --ip=$IP --port=$PORT
